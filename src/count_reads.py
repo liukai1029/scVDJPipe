29e8f016216813925ff56d3e6e05871e8ded5dab
kliu6 <kai.liu6@gilead.com>
2023-03-17 15:36:47 -0700

    calculate frequency by chain type at each well

--- src/count_reads.py
+++ src/count_reads.py
@@ -9,7 +9,8 @@ import numpy as np
 #wd = "/export/home/kliu6/projects/single_B_miseq/FASTQ/"
 #wd = "/export/home/kliu6/projects/single_B_miseq/rc_test/"
 #wd = "/export/home/kliu6/projects/single_B_miseq/FASTQ_human/"
-wd = "/export/home/kliu6/projects/single_B_miseq/FC1A/"
+#wd = "/export/home/kliu6/projects/single_B_miseq/FC1A/"
+wd = "/export/home/kliu6/projects/single_B_miseq/TL1A_BC003_genewiz_20230213/00_fastq/"
 
 
 
@@ -90,6 +91,7 @@ ordered_col = ['count',
 
 def count_reads(tsv_fp):
     df = pd.read_csv(tsv_fp, sep="\t")
+    logging.info(f"loaded data for: {input}")
     keep = df.copy()
     keep = keep[keep["stop_codon"]=="F"]
     keep = keep[keep["productive"]=="T"]
@@ -102,6 +104,7 @@ def count_reads(tsv_fp):
     keep = keep[~keep['cdr1_aa'].isna()]
     keep = keep[~keep['cdr2_aa'].isna()]
     keep = keep[~keep['cdr3_aa'].isna()]
+    logging.info(f"keep finished for: {input}")
     action = ['nunique'] + [lambda x:max(set(list(x)), key = list(x).count)]*(len(required_col)-1)
     vdj_keep = keep.groupby('sequence_alignment_aa').agg(dict(zip(required_col, action)))
     new_required_col = ['count' if ele=='sequence_id' else ele for ele in required_col]
@@ -109,13 +112,39 @@ def count_reads(tsv_fp):
     vdj_keep.sort_values(by='count', ascending=False, inplace=True)
     vdj_keep = vdj_keep[['*' not in ele for ele in vdj_keep['sequence_alignment_aa'].to_list()]]
     total_read_count = vdj_keep['count'].sum()
-    vdj_keep['frequency']=vdj_keep['count']/total_read_count
-    vdj_keep = vdj_keep[ordered_col]
+    #import pdb;pdb.set_trace()
+    #vdj_keep['frequency_by_well']=vdj_keep['count']/total_read_count
+    #vdj_keep = vdj_keep[ordered_col]
     vdj_keep.shape
     #vdj_keep.reset_index(inplace=True)
     fname = tsv_fp.split("/")[-1:][0]
     sname = fname.replace('.tsv','')
-    vdj_keep.index = [sname+'_'+str(ele) for ele in list(np.arange(1, len(vdj_keep) + 1))]
+    logging.info(f"start separating by chain type: {input}")
+    #import pdb; pdb.set_trace()
+    concat_list = []
+    if "IGH" in vdj_keep["locus"].to_list():
+        vdj_keep_h = vdj_keep[vdj_keep["locus"]=="IGH"]
+        vdj_keep_h.index = [sname+'_H_'+str(ele) for ele in list(np.arange(1, len(vdj_keep_h) + 1))]
+        total_read_count = vdj_keep_h['count'].sum()
+        vdj_keep_h['frequency']=vdj_keep_h['count']/total_read_count
+        vdj_keep_h = vdj_keep_h[ordered_col]
+        concat_list.append(vdj_keep_h)
+    if "IGK" in vdj_keep["locus"].to_list():
+        vdj_keep_k = vdj_keep[vdj_keep["locus"]=="IGK"]
+        vdj_keep_k.index = [sname+'_K_'+str(ele) for ele in list(np.arange(1, len(vdj_keep_k) + 1))]
+        total_read_count = vdj_keep_k['count'].sum()
+        vdj_keep_k['frequency']=vdj_keep_k['count']/total_read_count
+        vdj_keep_k = vdj_keep_k[ordered_col]
+        concat_list.append(vdj_keep_k)
+    if "IGL" in vdj_keep["locus"].to_list():
+        vdj_keep_l = vdj_keep[vdj_keep["locus"]=="IGL"]
+        vdj_keep_l.index = [sname+'_L_'+str(ele) for ele in list(np.arange(1, len(vdj_keep_l) + 1))]
+        total_read_count = vdj_keep_l['count'].sum()
+        vdj_keep_l['frequency']=vdj_keep_l['count']/total_read_count
+        vdj_keep_l = vdj_keep_l[ordered_col]
+        concat_list.append(vdj_keep_l)
+    vdj_keep = pd.concat(concat_list)
+    vdj_keep = vdj_keep.sort_values(by=['frequency'],ascending=False)
     rc_fp = tsv_fp.replace(".tsv","_rc.csv")
     vdj_keep.to_csv(rc_fp)
 
@@ -140,32 +169,9 @@ while fp:
     else:
         try:
             logging.info(f"start counting reads for: {input}")
+            #import pdb;pdb.set_trace()
             count_reads(input)    
             logging.info(f"complete counting reads for: {input}")
         except KeyError:
             logging.info(f"\n-\n-\n-\n-failed counting reads for: {input}\n-\n-\n-\n-")
             continue
-
-
-
-
-
-
-#if "IGH" in df["locus"].to_list():
-#    df_h = df[df["locus"]=="IGH"]
-#if "IGK" in df["locus"].to_list():
-#    df_k = df[df["locus"]=="IGK"]
-#if "IGL" in df["locus"].to_list():
-#    df_l = df[df["locus"]=="IGL"]
-
-
-
-
-
-
-
-#while fp:
-#    input = fp.pop(0)
-#    logging.info(f"start pyir for: {input}")
-#    call_pyir(input)    
-#    logging.info(f"complete pyir for: {input}")
